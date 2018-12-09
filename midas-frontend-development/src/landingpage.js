f95743766598560cac2e610b0d74e87a9a5adc0a
Patrick Vanegas <pvanny1124@gmail.com>
2018-12-09 02:34:55 -0500

    ...

--- midas-frontend-development/src/landingpage.js
+++ midas-frontend-development/src/landingpage.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import Home from "./components/Home";
-import ProfilePage from "./components/Profile-Page";
-import News from "./components/News";
+import Home from "./components/pages/Home-Page";
+import ProfilePage from "./components/pages/Profile-Page";
+import StockInfoPage from './components/pages/StockInfo-Page';
 import Navbar from "./components/CustomNavbar";
 import Footer from "./components/Footer";
 import Signup from "./components/Signup";
 import Signout from "./components/Signout";
 import Login from "./components/Login";
-import StockInfo from './components/StockInfo';
 import Leaderboards from './components/Leaderboards';
 import getStockInfo from './helpers/interactions/iex_interactions';
+import Footer from './com'
+
+
+//used to display previous state, current action, and next state
+import { logger } from 'redux-logger'; 
+
+//middleware package for async ops
+import thunk from 'redux-thunk';
+
+//main styles
 import './styles/style.css';
+
 
 
 class App extends Component {
@@ -26,6 +36,7 @@ class App extends Component {
   }
 
   componentWillMount(){
+    
     //check if user is already logged in session
     fetch("/api/auth")
       .then(response => {
@@ -34,13 +45,12 @@ class App extends Component {
       })
       .then(data => {
           this.setState({user: data, isLoading: false});
-          console.log("hi pat")
           console.log(data);
       })
       .catch(error => {
         setTimeout(() => {
           this.setState({isLoading: false})
-        }, 3000)
+        }, 1000)
         
           console.log(error);
       })
@@ -86,19 +96,17 @@ class App extends Component {
                 <Navbar user={this.state.user} forceUpdate={() => this.forceUpdate} getTicker={(ticker) => this.getSearchedTicker(ticker)}/>
                 <Route exact path="/" render={() => <Home user={this.state.user} getUser={(user) => this.getUserData(user)} />} />
                 {/* For the following view to render properly, pass key={props.location.key} to make the component re-render since the location changes if the user looks up a new stock*/}
-                <Route path="/stocks/:ticker" render={(props) => <div class="container stockInfo-page">
-                  <div className="page-left">
-                    <StockInfo key={props.location.key} {...props} reset={() => this.reset()} getUser={(user) => this.getUserData(user)} ticker={this.state.searchedTicker} user={this.state.user} getUser={(user) => this.getUserData(user)} />
-                  </div>
-                  <div className="page-right"></div>
-                </div>} />
+
+                <Route path="/stocks/:ticker" render={(props) => <StockInfoPage key={props.location.key} {...props} 
+                        reset={() => this.reset()} getUser={(user) => this.getUserData(user)} 
+                        ticker={this.state.searchedTicker} user={this.state.user} /> } />
+                
                 <Route path="/profile" render={() => <ProfilePage user={this.state.user} />} />
                 <Route path="/signup" render={() => <Signup getUser={(user) => this.getUserData(user)}/>} />
                 <Route path="/login" render={() => <Login getUser={(user) => this.getUserData(user)} />} />
                 <Route path="/signout" render={() => <Signout resetUser={() => this.resetUserData()} />} />
                 <Route path="/leaderboards" render={() => <Leaderboards />} />
-                <Route path="/news" component={News} />
-                {/* <Footer /> */}
+                <Footer />
              </div>
          ) }
          
